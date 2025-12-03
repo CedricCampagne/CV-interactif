b7287f000b644524bab8349722fa83726f138b25
CedricCampagne <ced.campagne@gmail.com>
2025-12-03 11:41:54 +0100

    Initial commit CV-interactif

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function ShowGrid(grid)
+{   
+    const divmain = document.querySelectorAll('.main_container_all');
+    
+    divmain.forEach(div =>{
+        div.style.display ="none";
+    })
+    
+    if(grid !== 'home'){
+        const description = document.querySelector('#description');
+        const nav = document.querySelector('.nav');
+
+        description.style.display='none';
+        nav.classList.add('open');
+        // nav.style.display='flex';
+    }else{
+        description.style.display='block';
+        nav.classList.remove('open');
+        // nav.style.display='none';
+    }
+
+    const gridToShow = document.getElementById(grid);
+    gridToShow.style.display = "grid";
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.querySelectorAll('.toggle_btn').forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+      const targetSelector = btn.getAttribute('data-target');
+      const target = document.querySelector(targetSelector);
+
+      if(target){
+        target.classList.toggle('open');
+        btn.classList.toggle('open');
+
+        // Si le contenu vient de s'ouvrir, on scroll dessus
+        if(target.classList.contains('open')){
+          target.scrollIntoView({
+            behavior: 'smooth',   // défilement fluide
+            block: 'center'        // aligne en haut de la fenêtre
+          });
+        }
+      }
+    });
+  });
+});
